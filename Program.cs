ef888d3f37a85cfbb1c2130a2cfcddef23dc0f3a
Simon R. <122377197+SaiJRZ12@users.noreply.github.com>
2025-09-22 22:34:46 +0200

    Second upload

---
+++ Program.cs
@@ -0,0 +1,38 @@
+using BlazorServer.Components;
+using BlazorServer.Components.Data;
+using BlazorServer.Components.Services;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseInMemoryDatabase("ProfesoresDB"));
+
+builder.Services.AddScoped<IProfesorService, ProfesorService>();
+
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseRouting();
+
+app.UseAntiforgery();
+
+app.MapStaticAssets();
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
